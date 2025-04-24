e310108cf009d6d96f7ee9f16bee6d805119508f
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-04-24 20:36:36 -0300

    chore: saving project

--- blazor-chart-webassembly/Program.cs
+++ blazor-chart-webassembly/Program.cs
@@ -2,6 +2,7 @@ using blazor_chart_webassembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
+
 
 await builder.Build().RunAsync();
